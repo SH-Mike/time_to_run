371733521cbd47577a273d071ff7df0d82e32844
Mike <michael.jaecques@outlook.fr>
2021-02-23 16:21:22 +0100

    feat: Added users handling in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,56 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\User;
+use Cocur\Slugify\Slugify;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private $userRepository;
+    private $slugify;
+    private $faker;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+        $this->slugify = new Slugify();
+        $this->faker = Factory::create();
+    }
+
+    /**
+     * We first load some random Users
+     * 
+     * @param ObjectManager
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUsers($manager, 10);
 
+        $manager->flush();
+    }
+
+    /**
+     * Creates $usersNb random Users
+     * 
+     * @param ObjectManager $manager
+     * @param int $usersNb
+     */
+    public function loadUsers($manager, $usersNb){
+        for ($i = 0; $i < $usersNb; $i++){
+            $user = new User();
+            $user->setFirstName($this->faker->firstName())
+            ->setLastName($this->faker->lastName())
+            ->setBirthDate($this->faker->dateTime('now'))
+            ->setEmail($this->faker->email())
+            ->setSlug($this->slugify->slugify($user->getFirstName() . " " . $user->getLastName()));
+            
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
