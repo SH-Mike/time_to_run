a17291226398ee990c56b4f1f8820c50ffd7f364
Mike <michael.jaecques@outlook.fr>
2021-02-25 20:23:47 +0100

    feat: Added user outings listing function

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\OutingRepository;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +26,58 @@ class ApiController extends AbstractController
         // Getting all the Outings
         $outings = $outingRepository->findAll();
 
+        if(!$outings){
+            return new Response('Aucune sortie n\'a été trouvée.', 404);
+        }
+
+        // We use JSON Encoder
+        $encoders = [new JsonEncoder()];
+
+        // We use a normalizer to convert collection into array
+        $normalizers = [new ObjectNormalizer()];
+
+        // We instantiate the converter
+        $serializer = new Serializer($normalizers, $encoders);
+
+        // We convert into JSON and instantiate the Response
+        $jsonOutings = $serializer->serialize($outings, 'json', [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);
+        $response = new Response($jsonOutings);
+
+        // We specify HTTP header request
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+        /**
+     * Api function to get all the Outings
+     * 
+     * @param OutingRepository $outingRepository
+     * @return Response
+     * 
+     * @Route("/api/outings/list/{user}", name="api_outings_user_list", methods={"GET"})
+     */
+    public function outingsUser(OutingRepository $outingRepository, User $user = null): Response
+    {
+        // We instantiate the error code
+        $error = 200;
+
+        if(!$user){
+            $error = 404;
+            return new Response('Cet utilisateur n\'existe pas.', $error);
+        }
+
+        // Getting all the Outings
+        $outings = $outingRepository->findBy(['user' => $user]);
+
+        if(!$outings){
+            $error = 404;
+            return new Response('Cet utilisateur n\'a effectué aucune sortie.');
+        }
         // We use JSON Encoder
         $encoders = [new JsonEncoder()];
 
