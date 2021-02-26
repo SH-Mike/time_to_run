23b76fa1679c3ebe317ad7bc7f66b27543eeeae1
Mike <michael.jaecques@outlook.fr>
2021-02-26 12:28:18 +0100

    fix: Added some comments and tests in OutingTypeController

--- src/Controller/OutingTypeController.php
+++ src/Controller/OutingTypeController.php
@@ -23,7 +23,16 @@ class OutingTypeController extends AbstractController
      */
     public function index(OutingTypeRepository $outingTypeRepository): Response
     {
-        $outingTypes = $outingTypeRepository->findAll();
+        //We get the outing types list
+        try{
+            $outingTypes = $outingTypeRepository->findAll();
+        }
+        catch(\Exception $e){
+            $this->addFlash("danger", "Une erreur est survenue lors de la récupération des types de sortie.");
+            return $this->redirectToRoute("homepage");
+        }
+
+        //We return the response page
         return $this->render('outing_type/index.html.twig', [
             'outingTypes' => $outingTypes,
         ]);
@@ -40,24 +49,35 @@ class OutingTypeController extends AbstractController
      * @Route("/outing-types/add", name="outing_type_add")
      */
     public function add(Request $request){
-
+        //Test if the user is connected
         if (!$this->getUser()) {
             $this->addFlash('danger', "Vous devez être connecté pour visiter cette page.");
             return $this->redirectToRoute('login');
         }
 
+        //We create the OutingType object and the form
         $outingType = new OutingType();
         $form = $this->createForm(OutingTypeType::class, $outingType);
+
+        //We handle the request and check if the form has been submitted
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($outingType);
-            $manager->flush();
+            //We try to add the outing type to the database and redirect the user depending on the result
+            try{
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($outingType);
+                $manager->flush();
+            }
+            catch(\Exception $e) {
+                $this->addFlash("danger", "Une erreur est survenue lors de l'ajout de ce type de sortie.");
+                return $this->redirectToRoute("outing_types_index");
+            }
             $this->addFlash('success', 'Votre type de sortie a bien été ajouté.');
             return $this->redirectToRoute('outing_types_index');
         }
 
+        //We return the response page
         return $this->render('outing_type/add.html.twig', [
             'form' => $form->createView(),
         ]);
@@ -75,29 +95,41 @@ class OutingTypeController extends AbstractController
      */
     public function editOuting(Request $request, OutingType $outingType = null): Response
     {
+        //Test if the user is connected
         if (!$this->getUser()) {
             $this->addFlash('danger', "Vous devez être connecté pour visiter cette page.");
             return $this->redirectToRoute('login');
         }
 
+        //Test if the outing type exists
         if (!$outingType) {
             $this->addFlash("danger", "Le type de sortie que vous souhaitez modifier n'existe pas.");
             return $this->redirectToRoute("outing_types_index");
         }
 
+        //We create the form
         $form = $this->createForm(OutingTypeType::class, $outingType);
 
+        //We handle the request and check if the form has been submitted
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $manager = $this->getDoctrine()->getManager();
-            $manager->persist($outingType);
-            $manager->flush();
+            //We try to edit the outing type in the database and redirect the user depending on the result
+            try{
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($outingType);
+                $manager->flush();
+            }
+            catch(\Exception $e) {
+                $this->addFlash("danger", "Une erreur est survenue lors de la modification de ce type de sortie.");
+                return $this->redirectToRoute("outing_types_index");
+            }
 
             $this->addFlash("success", "Votre type sortie a bien été modifié.");
             return $this->redirectToRoute("outing_types_index");
         }
 
+        //We return the response page
         return $this->render('outing_type/edit.html.twig', [
             'form' => $form->createView(),
             'outingType' => $outingType,
@@ -115,16 +147,19 @@ class OutingTypeController extends AbstractController
      */
     public function deleteOuting(OutingType $outingType = null): Response
     {
+        //Test if the user is connected
         if (!$this->getUser()) {
             $this->addFlash('danger', "Vous devez être connecté pour visiter cette page.");
             return $this->redirectToRoute('login');
         }
 
+        //Test if the outing type exists
         if (!$outingType) {
             $this->addFlash("danger", "Le type de sortie que vous souhaitez supprimer n'existe pas.");
             return $this->redirectToRoute("outing_types_index");
         }
 
+        //We try to delete the outing type from the database and redirect the user depending on the result
         try{
             $manager = $this->getDoctrine()->getManager();
             $manager->remove($outingType);
@@ -135,6 +170,7 @@ class OutingTypeController extends AbstractController
             return $this->redirectToRoute("outing_types_index");
         }
 
+        //No response page here, we redirect the user to the outing types index page
         $this->addFlash("success", "Le type de sortie a bien été supprimée.");
         return $this->redirectToRoute("outing_types_index");
     }
